30c9c8418a8f5148f294b25a56c23173ecaa6781
ahnch <ahnch@DESKTOP-ISSSQV3>
2019-07-10 20:16:48 +0900

    Ahn : Mapper, DAO, impl, test (2019/07/10 20:16)

--- VisionPJT/src/main/java/com/vision/erp/service/approval/ApprovalDAO.java
+++ VisionPJT/src/main/java/com/vision/erp/service/approval/ApprovalDAO.java
@@ -2,7 +2,10 @@ package com.vision.erp.service.approval;
 
 import java.util.List;
 
+import com.vision.erp.common.Search;
+import com.vision.erp.service.domain.Approval;
 import com.vision.erp.service.domain.ApprovalForm;
+import com.vision.erp.service.domain.Approver;
 
 public interface ApprovalDAO {
 
@@ -14,18 +17,40 @@ public interface ApprovalDAO {
 	public int insertApprovalForm(ApprovalForm approvalForm) throws Exception;
 	
 	//결재서양식 수정하기
-	public int updateApprovalForm() throws Exception;
+	public int updateApprovalForm(ApprovalForm approvalForm) throws Exception;
 	
 	//결재서양식 상세보기
-	public ApprovalForm selectApprovalFormDetail() throws Exception;
+	public ApprovalForm selectApprovalFormDetail(String approvalFormNo) throws Exception;
 	
 	//결재서양식 삭제, 복구하기
-	public int updateApprovalFormUsageStatus() throws Exception;
+	public int updateApprovalFormUsageStatus(ApprovalForm approvalForm) throws Exception;
 
 	//결재서양식 useCount 올리기
-	public int updateApprovalFormUseCount() throws Exception;
+	public int updateApprovalFormUseCount(String approvalFormNo) throws Exception;
 	
 	/////////////////////결재/////////////////////////////
+	//결재상신
+	public int insertApproval(Approval approval) throws Exception;
 	
+	//결재자 등록하기
+	public int insertApprover(Approver approver) throws Exception;
+	
+	//결재자 가져오기
+	public List<Approver> selectApproverList(String approvalNo) throws Exception;
+	
+	//결재목록 가져오기(상신, 반려, 완료, 대기)
+	public List<Approval> selectApprovalList(Search search) throws Exception;
+	
+	//결재서 상세보기
+	public Approval selectApprovalDetail(String approvalNo) throws Exception;
+	
+	//결재서상태 변경하기(진행, 완료, 반려)
+	public int updateApprovalStatus(Approval approval) throws Exception;
+	
+	//결재자의 결재상태 변경하기(승인, 반려)
+	public int updateApproverStatus(Approver approver) throws Exception;
+	
+	//승인한결재자수 변경하기
+	public int updateApproverCountFromApproval(String approvalNo) throws Exception;
 	
 }
